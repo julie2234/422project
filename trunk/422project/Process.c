11b6f1388e72dadbca35c7e2acd4f42a908d63c1
tsim23@uw.edu <tsim23@uw.edu@461eec6b-4ce2-31e6-1a83-f265acc2a3fe>
2013-05-18 20:28:28 +0000

    Began the process ADT.

--- trunk/422project/Process.c
+++ trunk/422project/Process.c
@@ -8,9 +8,8 @@
 #include "Process.h"
 #include <stdio.h>
 
-int main (int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    printf("Testing from Eclipse")
+int main (int argc, const char * argv[])
+{
+
     return 0;
 }
