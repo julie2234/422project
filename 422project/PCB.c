7e657d9855e1f2911e6a021265da231845a5220a
trottr <trottr@uw.edu>
2013-05-27 22:52:24 +0000

    Added a pcbIterator() that the CPU can call for each tick through the data.

--- 422project/PCB.c
+++ 422project/PCB.c
@@ -24,4 +24,24 @@ PcbPtr pcbConstruct(int the_PID, int the_processType) {
 	return temp_pcb;
 }
 
+/************************************************************
+* This code is added for processing data after the DeviceIO *
+* has initialized the PCB and has gone through the ready    *
+* queue and is ready to be processed.                       *
+* This method is called by the CPU until the data has been  *
+* fully processed, or until an interupt occurse.            *
+* By using this method the thread running in the PCB does   *
+* not get interupted but the CPU its self is interupted     *
+************************************************************/
+
+int pcbIterator() {
+    int boolean = temp_pcb->count - temp_pcb->currentCount;
+    if (boolean > 0) {
+       temp_pcb->clock_counter++;  
+       // Input code hear if you wish to process something.
+       // Currently just iterates through the data and does nothing.                        
+    }
+    return boolean; //returns true if the PCB is not completely processed.
+}
+
 
