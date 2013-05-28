6e47e84bff8c64af15f3e0ea6af582e99e1af5b6
tsim23 <tsim23@uw.edu>
2013-05-28 19:25:27 +0000

    timer update.

--- 422project/timer.c
+++ 422project/timer.c
@@ -16,12 +16,12 @@ int continue_ticks = 1;
 
 //This function goes to sleep for the specified quanta, wakes up, calls a function,
 //and goes back to sleep continuously until the program stops it from sleeping.
-void *tickingTimer(int quanta)
+void *tickingTimer(void* quanta)
 {
 	int loops = 0;
 	while(continue_ticks)
 	{
-		sleep(quanta);
+		sleep((int) quanta);
 		timeEndInterrupt();
 		//notify CPU/Controller, maybe something like
 		//interruptNotify() that would simply point out that it is
