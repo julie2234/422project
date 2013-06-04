dc31c5c7c3e05573b3c0f44af22c2402abb78bae
trottr@uw.edu <trottr@uw.edu@461eec6b-4ce2-31e6-1a83-f265acc2a3fe>
2013-06-04 20:16:40 +0000

    Changes to Device_io.c

--- trunk/422project/device_io.c
+++ trunk/422project/device_io.c
@@ -5,17 +5,16 @@
  *      TCSS 422, Spring 2013
  */
 
-
 #include "device_io.h"
 
-DeviceIO initialize() {
+DeviceIO device_io_contrustor(int io_interrupt_id, int io_process_id) {
        srand(time(NULL));
        
        DeviceIO temp_dev_io = (DeviceIO) malloc(sizeof(DeviceIOStr));
-       temp_dev_io->io_interrupt_current_count = 0; 
+       temp_dev_io->io_interrupt_current_count = 0;
        temp_dev_io->io_interrupt_time = MIN_INTERRUPT_DELAY + rand() % MAX_INTERRUPT_DELAY;
-       temp_dev_io->device_io_interrupt = 1;
-       temp_dev_io->device_io_activity = 1;
+       temp_dev_io->device_io_interrupt = io_interrupt_id;
+       temp_dev_io->device_io_process = io_process_id;
        return temp_dev_io;
 }
 
@@ -29,34 +28,25 @@ DeviceIO devioReActivate(DeviceIO temp_dev_io) {
 }
 
 //Interrupts the CPU
-void interruptCpu() {
-     //CHECK TO SEE IF CPU IS ALREADY INTERRUPTED
-     //setInterrupt(1, process_waiting);
+void interruptCpu(DeviceIO temp_dev_io) {
+     setInterrupt(temp_dev_io->device_io_interrupt,  temp_dev_io->device_io_process);
 }
 
 //a looping thread that runs until device io activity is set to 0
-static void *deviceLoop() {
-     DeviceIO temp_dev_io;
-     temp_dev_io = initialize();
-     while(temp_dev_io->device_io_activity) {
-          while (++temp_dev_io->io_interrupt_current_count != temp_dev_io->io_interrupt_time) {
-                temp_dev_io->io_interrupt_current_count = temp_dev_io->io_interrupt_current_count % MAX_INTERRUPT_DELAY;
-          }
-          interruptCpu();
-          temp_dev_io = devioReActivate(temp_dev_io);
-     }  
-     return 0;
-}
-
-//Stops/Kills the running thread
-void killDeviceIO(DeviceIO temp_dev_io) {
-     temp_dev_io->device_io_activity = 0;
+static void *deviceLoop(void* temp_dev_io_ptr) {
+       DeviceIO temp_dev_io = (DeviceIO) temp_dev_io_ptr;
+       while (temp_dev_io->io_interrupt_current_count++ != temp_dev_io->io_interrupt_time) {
+             temp_dev_io->io_interrupt_current_count = temp_dev_io->io_interrupt_current_count % MAX_INTERRUPT_DELAY;
+       }
+       interruptCpu(temp_dev_io);
+       return 0;
 }
 
 //Initializes the pthread
-void startDeviceIO() {
+void startDeviceIO(DeviceIO temp_dev_io) {
+     devioReActivate(temp_dev_io);
      pthread_t device_thread;
-     pthread_create(&device_thread, NULL, deviceLoop, NULL);
+     pthread_create(&device_thread, NULL, deviceLoop, (void *) temp_dev_io;
 }
 
 
