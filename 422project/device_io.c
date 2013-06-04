4c66ef4e1a19e3c894225022ffaaf7461fb22ca0
trottr <trottr@uw.edu>
2013-06-04 15:06:57 +0000

    minor changes. cleaned up device_io.c

--- 422project/device_io.c
+++ 422project/device_io.c
@@ -1,71 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
 #include <pthread.h>
 #include "device_io.h"
-//#include "CPU.h"
 
 DeviceIO initialize() {
        srand(time(NULL));
        
        DeviceIO temp_dev_io = (DeviceIO) malloc(sizeof(DeviceIOStr));
        temp_dev_io->io_interrupt_current_count = 0; 
-       temp_dev_io->io_interrupt_time = 100 + rand() % MAX_INTERRUPT_DELAY;
-       temp_dev_io->io_interrupt = 1;
+       temp_dev_io->io_interrupt_time = MIN_INTERRUPT_DELAY + rand() % MAX_INTERRUPT_DELAY;
+       temp_dev_io->device_io_interrupt = 1;
        //temp_dev_io->io_cpu = cpu;
        return temp_dev_io;
 }
 
-
-
-void devioReActivate(DeviceIO temp_dev_io) {
-     temp_dev_io->io_interrupt_time = 100 + rand() % MAX_INTERRUPT_DELAY;
+DeviceIO devioReActivate(DeviceIO temp_dev_io) {
+     temp_dev_io->io_interrupt_time = MIN_INTERRUPT_DELAY + rand() % MAX_INTERRUPT_DELAY;
      temp_dev_io->io_interrupt_current_count = 0;
-}
-/*
-void devLoop(DeviceIO temp_dev_io) {
-     while(++temp_dev_io->io_interrupt_current_count != temp_dev_io->io_interrupt_time) {
-          temp_dev_io->io_interrupt_current_count = temp_dev_io->io_interrupt_current_count % MAX_INTERRUPT_DELAY;
-     }
-     //interrupt_cpu();
+     return temp_dev_io;
 }
 
-void int interruptCpu(DeviceIO temp_dev_io) {
-     cpu->iterruptFlag = temp_dev_io->io_interrupt;
-     
+void interruptCpu() {
+     //CHECK TO SEE IF CPU IS ALREADY INTERRUPTED
+     setInterrupt(1, process_waiting);
 }
-*/
-static void *deviceLoop(void* num) {
+
+static void *deviceLoop() {
      DeviceIO temp_dev_io;
      temp_dev_io = initialize();
-     //while() {
-          while(++temp_dev_io->io_interrupt_current_count != temp_dev_io->io_interrupt_time) {
-               printf("%d",temp_dev_io->io_interrupt_current_count);
-               temp_dev_io->io_interrupt_current_count = temp_dev_io->io_interrupt_current_count % MAX_INTERRUPT_DELAY;
-          }  
-     //}
+     while(++temp_dev_io->io_interrupt_current_count != temp_dev_io->io_interrupt_time || temp_dev_io->device_io_interrupt) {
+          printf("%d",temp_dev_io->io_interrupt_current_count);
+          temp_dev_io->io_interrupt_current_count = temp_dev_io->io_interrupt_current_count % MAX_INTERRUPT_DELAY;
+     }  
+     interruptCpu();
      return 0;
 }
 
-void startDeviceIO(int num) {
+void startDeviceIO() {
      pthread_t device_thread;
-     pthread_create(&device_thread, NULL, deviceLoop, (void *) num);
+     pthread_create(&device_thread, NULL, deviceLoop, NULL);
 }
 
 int main(void) {
-      startDeviceIO(5);
+      startDeviceIO();
       pthread_exit(NULL);
 }
-
-/*
-int increment(int data_count) {
-    temp_dev_io->current_count = ++temp_dev_io->current_count % MAX_INTERRUPT_DELAY;
-    if(temp_dev_io->current_count == 0) {
-          temp_dev_io->current_count = 1;
-     }
-     if (temp_dev_io->current_count == temp_dev_io->io_interrupt_time) {
-          interrupt_cpu(temp_dev_io);                       
-     }
-    return data_count;
-}
-*/
