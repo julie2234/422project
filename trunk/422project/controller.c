3146bda0427699a0a976d15d993f9441050631ed
tsim23@uw.edu <tsim23@uw.edu@461eec6b-4ce2-31e6-1a83-f265acc2a3fe>
2013-06-05 21:20:35 +0000

    Ok, this is probably a change no one wanted me to make. You MUST run this in a terminal now unless your IDE supports command line arguments (all IDEs probably do, but I find it easier to just to compile it then run it in a terminal). Command line arguments have been implemented without too much trouble. If you enter more than 2 IO devices, I have it scale it down to just two, same goes for KB IO.

--- trunk/422project/controller.c
+++ trunk/422project/controller.c
@@ -23,21 +23,54 @@ ControllerPtr controllerConstruct() {
 }
 
 
-int main ()
+int main (int argc, char* argv[])
 {
+	int process_num;
+	int kb_num;
+	int io_num;
+	int pc_size;
+
+	if(argc == 9)
+	{
+		//printf("%s ", argv[1]);// -p
+		process_num = (int) strtol(argv[2], NULL, 10);
+		//printf("%d\n", process_num);// num of -p
+
+		//printf("%s ", argv[3]);// -k
+		kb_num = (int) strtol(argv[4], NULL, 10);
+		//printf("%d\n", kb_num);// num of -k
+
+		//printf("%s ", argv[5]); //-io
+		io_num = (int) strtol(argv[6], NULL, 10);
+		//printf("%d\n", io_num);
+
+		//printf("%s ", argv[7]);// -pc
+		pc_size = (int) strtol(argv[8], NULL, 10);
+		//printf("%d\n", pc_size);
+	}
+	else
+	{
+		printf("You input the incorrect parameters. To run this program, it must have "
+				"these input paramters: \n"
+				"-p x -k x -io x -pc x\n"
+				"where 'x' is a number");
+		return 0;
+	}
+
 	ControllerPtr controller = controllerConstruct();
 	srand(time(NULL)); //Seeds the random number generator
+	createProcesses(process_num, kb_num, io_num, controller);
 
-	//just for testing
-	controller->processList[0] = pcbConstruct(0, 1);
-	controller->processList[1] = pcbConstruct(1, 3);
-	controller->processList[2] = pcbConstruct(2, 2);
-	controller->processList[3] = pcbConstruct(3, 4);
+//	//just for testing
+//	controller->processList[0] = pcbConstruct(0, 1);
+//	controller->processList[1] = pcbConstruct(1, 3);
+//	controller->processList[2] = pcbConstruct(2, 2);
+//	controller->processList[3] = pcbConstruct(3, 4);
 	
 
 	printf("Process Summary\n");
 	int i;
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < process_num; i++)
 	{
 		int type = controller->processList[i]->processType;
 		printf("P%d is type %d and has service call values", controller->processList[i]->PID, type);
@@ -58,6 +91,7 @@ int main ()
 	cpuRun(cpu);
 
 	pthread_exit(NULL);
+	return 0;
 }
 
 
@@ -117,6 +151,37 @@ void setProcessReady(ControllerPtr controller, int IODeviceID)
                 Queue_add(controller->readyQueue, temp_pcb);
                 printf("Process %d removed from video blocked queue and added to the ready queue\n", temp_pcb->PID);
         }
+}
+
+void createProcesses(int total_processes, int kb_amount, int io_amount, ControllerPtr controller)
+{
+	controller->processList = (PcbPtr*) malloc((total_processes + 1) * sizeof(PcbPtr));
+	int cpu_bound_processes = total_processes - kb_amount - io_amount;
+	int processID = 0;
+	int index = 0;
+	for(; index < cpu_bound_processes; index++)
+	{
+		controller->processList[processID] = pcbConstruct(processID, 0);
+		processID++;
+	}
+
+	if(kb_amount > 0)
+	{
+		controller->processList[processID] = pcbConstruct(processID, 1);
+		processID++;
+	}
+
+	if(io_amount > 2)
+	{
+		io_amount = 2;
+	}
+
+	for(index = 0; index < io_amount; index++)
+	{
+		//process types for IO start at 2, so index has two added to it for the correct io type
+		controller->processList[processID] = pcbConstruct(processID, index + 2);
+		processID++;
+	}
 }
 
 
