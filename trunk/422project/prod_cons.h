d146a853cc680991d9dcb39cfb548d2b5c2a9d2c
mmm46@uw.edu <mmm46@uw.edu@461eec6b-4ce2-31e6-1a83-f265acc2a3fe>
2013-06-06 18:57:52 +0000

    edited for correctness

--- trunk/422project/prod_cons.h
+++ trunk/422project/prod_cons.h
@@ -1,6 +1,16 @@
 #ifndef PROD_CONS_H
 #define PROD_CONS_H
 
+#include "CPU.h"
+#include "mutex.h"
+
+// Global "shared" memory.
+int mem;
+// Global condition variable.
+int new_mem;
+// Shared memory mutex.
+struct mutex_str mem_mut;
+
 // nothing
 void Producer_tick(CpuPtr);
 void Consumer_tick(CpuPtr);
